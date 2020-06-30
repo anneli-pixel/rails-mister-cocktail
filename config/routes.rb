59c0ac8f80d43a079e55e98a4fef0be80211597b
Anne Lißner <anne_lissner@gmx.net>
2020-06-30 21:28:23 +0200

    added new and create routes for review

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   root to: "cocktails#index"
   resources :cocktails, only: [:index, :show, :new, :create, :destroy] do
     resources :doses, only: [:new, :create, :show]
+    resources :reviews, only: [:new, :create]
   end
 
   resources :doses, only: :destroy
+
 end
 
